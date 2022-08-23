cebad9d4cedeec71037b6a3509bd2c2eb288c47d
rberecka <rachael.berecka@hashicorp.com>
2022-08-23 13:51:02 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -3,9 +3,11 @@ output "story" {
           Decisions can be hard. You might even say fatiguing. 
           These are the ones I've made for you so you don't have to!
 
-          * You should get a ${random_pet.pet.id} for a pet and name it ${var.favourite_author["first_name"]} ${var.first_teacher_last_name}.
+          * You should get a ${random_pet.pet.id} for a pet 
+            and name it ${var.favourite_author["first_name"]} ${var.first_teacher_last_name} von ${var.current_street}.
           * Go with the ${var.favourite_color} shirt today.
-          * Have ${random_shuffle.icecream_choice.result[0]} iceream for dinner.
-          * Hahaha. Not gonna be tricked to share your secret.
+          * Can you have iceream today? ${var.have_icecream}
+          * If true, have ${random_shuffle.icecream_choice.result[0]} iceream.
+          * Keep that secret secret! Don't tell it to strangers on the internet!
           EOT
 }
