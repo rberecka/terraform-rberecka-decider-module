4162aaafbbc6b960782d8e3a90bace527a5a0a33
rberecka <rachael.berecka@hashicorp.com>
2022-08-25 15:37:43 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,10 +1,10 @@
 output "story" {
- value = <<EOT
+ value = nonsensitive(<<EOT
           Decisions can be hard. You might even say fatiguing. 
           These are the ones I've made for you so you don't have to!
 
           * You should get a ${random_pet.pet.id} for a pet and name it ${var.favourite_author["first_name"]} ${var.first_teacher_last_name} von ${var.street_name}.
           * Go with the ${var.favourite_color} shirt today.
           * Have ${random_shuffle.icecream_choice.result[0]} iceream for dinner.
-          EOT
+          EOT)
 }
